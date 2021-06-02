fc2b8f18db95bd5ea2b1409e24c3a12f8b9e97fc
konrad <ktalaga90@gmail.com>
2021-06-02 20:22:36 +0100

    repositories amended

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository.java
@@ -1,5 +1,9 @@
 package com.codeclan.example.WhiskyTracker.repositories;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DistilleryRepository  {
+@Repository
+public interface DistilleryRepository extends JpaRepository<Distillery, Long> {
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -1,5 +1,10 @@
 package com.codeclan.example.WhiskyTracker.repositories;
 
-public interface WhiskyRepository {
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
 
 }
