22d8bdc24949371c6cb3d3f12c8b3d2c98718c98
konrad <ktalaga90@gmail.com>
2021-06-02 20:12:06 +0100

    project started

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/models/Distillery.java
@@ -0,0 +1,57 @@
+package com.codeclan.example.WhiskyTracker.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Distillery {
+
+    private Long id;
+
+    private String name;
+
+    private String region;
+
+    private List<Whisky> whiskies;
+
+    public Distillery(String name, String region) {
+        this.name = name;
+        this.region = region;
+        this.whiskies = new ArrayList<>();
+    }
+
+    public Distillery() {
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public List<Whisky> getWhiskies() {
+        return whiskies;
+    }
+
+    public void setWhiskies(List<Whisky> whiskies) {
+        this.whiskies = whiskies;
+    }
+}
---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository.java
@@ -0,0 +1,5 @@
+package com.codeclan.example.WhiskyTracker.repositories;
+
+
+public interface DistilleryRepository  {
+}
